df0c2a5859cf86cf9fe05aa25c8e77b635d16b29
Lloyd <UpstateLloyd@gmail.com>
2021-03-23 15:27:16 -0400

    First working version of appstate service.  Need to get IConfig working inside service.

--- Services/PeopleServices.cs
+++ Services/PeopleServices.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ComponentCommunication.Data;
 using ComponentCommunication.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Configuration;
 
 namespace ComponentCommunication.Services
@@ -11,14 +12,14 @@ namespace ComponentCommunication.Services
     public class PeopleServices
     {
         public IDataAccess _data = new DataAccess();
-        public IConfiguration _config;
+        public IConfiguration _config { get; }
         public async Task<List<Person>> ListPeople()
         {
             string sql = "SELECT * FROM people";
 
-//            List<Person> people = new List<Person>();
+            //List<Person> people = new List<Person>();
 
-            return await _data.LoadData<Person, dynamic>(sql, new { }, _config.GetConnectionString("default"));
+            return await _data.LoadData<Person, dynamic>(sql, new { }, "Server=localhost;Port=3306;database=ytdemo;user id=upstatedashadmin;password=LH85upst4t3d4sh!");// Startup.Configuration.GetConnectionString("default"));
 
         }
         public async Task InsertData(Person person)
@@ -30,7 +31,7 @@ namespace ComponentCommunication.Services
                 FirstName = person.FirstName,
                 LastName = person.LastName,
                 Email = person.Email
-            }, _config.GetConnectionString("default"));
+            }, "Server=localhost;Port=3306;database=ytdemo;user id=upstatedashadmin;password=LH85upst4t3d4sh!");// _config.GetConnectionString("default"));
 
 
         }
